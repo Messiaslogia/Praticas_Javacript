1fe910fa913340a07e485fef2435801f03619ec0
unknown <messias.logia@gmail.com>
2022-02-23 02:42:53 -0300

    Variaveis

---
+++ javascript/fundamentos/tipagemFraca.js
@@ -0,0 +1,15 @@
+/**
+ * JavaScript é fracamente tipada
+ */
+let qualquer = 'Legal'
+console.log(qualquer)
+console.log(typeof qualquer)
+
+qualquer - 3.1415
+console.log(qualquer)
+console.log(typeof qualquer)
+
+//Evitar nome genérico 
+let valor = ''
+let numero = 1
+let pqp = false  //Produto Quimico Perigoso...kkk
