42f81e46b6f3d21f311a38421ac3c3e5ea80c14f
messiaslogia <messias.logia@gmail.com>
2022-05-19 13:18:41 -0300

    Exercicios parte 2

---
+++ exercicios/2.0-exercicios/exercicio-1.js
@@ -0,0 +1,11 @@
+/**
+ * Crie uma função que retorna a string "Olá, " concatenada com um argumento text (a ser passado para a função) e
+com ponto de exclamação "!" no final.
+*/
+
+function compliment(name){
+
+    return `Olá ${name}!`
+}
+
+console.log(compliment('Messias'))
---
+++ exercicios/2.0-exercicios/exercicio-2.js
@@ -0,0 +1,11 @@
+/**
+ * Escreva uma função que receba a idade de uma pessoa em anos e retorne a mesma idade em dias.
+ */
+
+function ageToDays(age){
+
+    const converter = age * 366
+    console.log(converter)
+}
+
+ageToDays(5)
