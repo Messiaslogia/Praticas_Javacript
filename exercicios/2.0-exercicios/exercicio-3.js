81b3fde04193f588b7982ae7cf374ef7bdc20f59
messiaslogia <messias.logia@gmail.com>
2022-05-20 13:05:00 -0300

    Exercicios 20

---
+++ exercicios/2.0-exercicios/exercicio-3.js
@@ -0,0 +1,17 @@
+/**
+ * Desenvolva uma função que recebe dois parâmetros, um é a quantidade de horas trabalhadas por um funcionário
+num mês, e o quanto ele recebe por hora. A função deverá calcular o salário líquido mensal do funcionário, que é
+da quantidade de horas trabalhadas no mês multiplicada pelo valor da sua hora. Desse valor, deve ser subtraído
+30%, relativo a impostos
+ */
+
+function salary(hoursWorked, salaryHours){
+
+    const salaryGross = hoursWorked * salaryHours
+    const salaryLiquid = salaryGross - salaryGross * 30/(100)
+
+    return `Salário igual a R$ ${salaryLiquid}`
+
+}
+
+console.log(salary(180, 60))
