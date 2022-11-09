ebbd0a3e106eea1e8967be53680a41519e32687e
messiaslogia <messias.logia@gmail.com>
2022-11-09 18:07:20 -0300

    Começando Node

---
+++ node/moduloA.js
@@ -0,0 +1,6 @@
+console.log(this)
+
+// Jeitos de exportar os modulos
+this.ola = 'Fala pessoal';
+exports.bemVindo = 'Bem vindo ao node!'
+module.exports.ateLogo = 'Até a proxima'
---
+++ node/moduloB.js
@@ -0,0 +1,8 @@
+// Modo padrão de Exportação
+
+module.exports = {
+    bomDia: 'bom dia',
+    boaNoite() {
+        return 'Boa noite'
+    }
+}
