8d2b931f9652bad9c0f8cd8aea9b52ea8b8e5427
messiaslogia <messias.logia@gmail.com>
2023-02-07 16:37:09 -0300

    Express - Finalizado

--- exercicios-express/index.js
+++ exercicios-express/index.js
@@ -1,9 +1,48 @@
 const express = require('express')
 const app = express()
+const bodyParser = require('body-parser')
+
+const saudacao = require('./saudacaoMid')
+const usuarioApi = require('./api/usuario')
+const produtoApi = require('./api/produto')
+produtoApi(app, ' com param')
+
+app.post('/usuario', usuarioApi.salvar)
+app.get('/usuario', usuarioApi.obter)
+
+app.use(bodyParser.text())
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+
+
+
+app.use(saudacao('Guilherme'))
 
 app.use('/opa', (req, res, next) => {
     console.log('Antes...')
     next()
+})
+
+app.get('/clientes/relatorio', (req, res) => {
+    res.send(`Cliente relatorio: completo = ${req.query.completo} ano = ${req.query.ano}`)
+})
+
+app.post('/corpo', (req, res) => {
+    // let corpo = ''
+    // req.on('data', function(parte) {
+    //     corpo += parte
+    // })
+
+    // req.on('end', function() {
+    //     res.send(corpo)
+    // })
+
+    res.send(req.nome)
+})
+
+app.get('/clientes/:id', (req, res) => {
+    res.send(`Cliente ${req.params.id} selecionado!`)
+
 })
 
 app.use('/opa', (req, res, next) => {
@@ -36,6 +75,6 @@ app.use('/opa', (req, res, next) => {
     next()
 })
 
-app.listen(3001, () => {
+app.listen(3002, () => {
     console.log("Executando 2...")
 })
